5c281228598d360abf04b4d65b11e413ec178b38
Bruno Duarte <duartebruno2208@gmail.com>
2023-09-24 14:53:55 -0300

    feat: app layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+export const Header = () => {
+  return (
+    <header className="App-header">
+      <Link to="/">
+        Fast React Pizza
+      </Link>
+    </header>
+  )
+}
