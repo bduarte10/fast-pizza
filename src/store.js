c3500b66afbcb1aa5e595dfe5708996ecebf1c4d
Bruno Duarte <duartebruno2208@gmail.com>
2023-10-30 01:01:10 -0300

    fix: dependencies

--- src/store.js
+++ src/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/user/userSlice";
 import cartReducer from "./features/cart/cartSlice";
-import favReducer from "./features/favorites/favSlice";
 
  const store = configureStore({
   reducer: {
     user: userReducer,
     cart: cartReducer,
-    fav: favReducer
   },
 });
 
